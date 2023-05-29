ff9361efd60bc2276e373d65f6124851bebd0431
Adrian1706 <adrianjbuenop@gmail.com>
2023-05-29 17:20:39 -0500

    feat: :construction: Punto 7
    Punto 7 de la iniciacion de PHP

--- index.php
+++ index.php
@@ -5,4 +5,14 @@
     printf("Hola %s",$texto);   /** El printf Esta función es similar a echo, pero solo puede imprimir una cadena de texto a la vez */
     $Mensaje = sprintf("Hola %s",$texto); /** El sprintf es lo mismo que un printf, pero esta etiqueta n lugar de imprimir la cadena formateada en la salida estándar, devuelve la cadena formateada como resultado */
     echo $Mensaje;
+
+
+    $edad = 17; /** Variable numerica */
+    $nombre = "Adrian"; /** Variable de texto */
+    $es_valido = true; /** Variable booleana */
+    echo var_dump($nombre);
+
+    define("PI", 3.1416); /** Constante numerica */
+    edfine("SALUDO", "Hola Mundo!"); /** Constante de texto */
+    define("ES_VALIDO", true); /** constante boolean */
 ?>
