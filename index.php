05b297cb4abb3ba3a2662942ba44f3e1412e3d8e
Adrian1706 <adrianjbuenop@gmail.com>
2023-05-29 17:14:11 -0500

    feat: :construction: Punto 6
    este es el punto 6 de la iniciacion de PHP

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php /** Esta etiqueta es para indicar que es de php, asi como en javascript es script y asi como en css es link */
-    echo "<h1>Hola mundo cruel</h1>" /** la etiqueta echo es para imprimir en php*/
+    echo 'Texto a Imprimir'; /** la etiqueta echo es para imprimir en php*/
+
+    $texto = "Mundo"; /** Esta es la forma de crear una variable en PHP */
+    printf("Hola %s",$texto);   /** El printf Esta función es similar a echo, pero solo puede imprimir una cadena de texto a la vez */
+    $Mensaje = sprintf("Hola %s",$texto); /** El sprintf es lo mismo que un printf, pero esta etiqueta n lugar de imprimir la cadena formateada en la salida estándar, devuelve la cadena formateada como resultado */
+    echo $Mensaje;
 ?>
