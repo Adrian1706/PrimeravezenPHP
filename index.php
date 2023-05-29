a033b65b626a7e4569a96bea4fbd2ad5486b303f
Adrian1706 <adrianjbuenop@gmail.com>
2023-05-29 17:05:32 -0500

    feat: :construction: Punto 5
    Este es el punto 5 de la inicializacion de PHP

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php /** Esta etiqueta es para indicar que es de php, asi como en javascript es script y asi como en css es link */
+    echo "<h1>Hola mundo cruel</h1>" /** la etiqueta echo es para imprimir en php*/
+?>
